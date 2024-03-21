25c81bd556d72efc7151dbc2bf58ad2db51274ef
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-20 21:50:14 -0300

    Update questao20.py

--- questao20.py
+++ questao20.py
@@ -1,1 +1,14 @@
+def main():
+  print('Progressão Aritmética\n------------')
+  a0 = int(input('Digite o Início da Progressão : '))
+  razao = int(input('Digite a Razão Da progressão : '))
+  n = int(input('Digite a Quantidade de Termos :  '))
 
+  contador = 0
+  print(a0)
+  while contador <= n:
+    a0 += razao
+    contador += 1 
+    print(a0)
+
+main()
