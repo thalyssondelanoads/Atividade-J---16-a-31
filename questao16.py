3757c9720641b4803999a8496d739ea83f33ca08
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-28 20:48:19 -0300

    Update questao16.py

--- questao16.py
+++ questao16.py
@@ -1,1 +1,14 @@
+def main():
+  print('Pagamento de Juros\nJuros de 0.85% ao Dia\n------------------')
+  
+  valor_emprestimo = float(input('Digite o Valor do Empréstimo : '))
+  pagamento_diario = float(input('Digite o Valor do Pagamento Diário : '))
+  
+  dias = 0
+  while valor_emprestimo > 0:
+    dias += 1
+    valor_emprestimo -= pagamento_diario
+    valor_emprestimo *= 1.0085
+  print(f'Você Pagará o Empréstimo em {dias} Dias')
 
+main()
