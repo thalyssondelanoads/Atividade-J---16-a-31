34d242036c284ed45462aeeee67131cd21e8bec8
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-28 20:17:55 -0300

    Update questao21.py

--- questao21.py
+++ questao21.py
@@ -1,1 +1,14 @@
+def main():
+  print('Multiplicação sem Operador\n-----------------')
+  print('Escreva os 2 Números que Deseja Multiplicar')
+  n1 = int(input('Primeiro Número: '))
+  n2 = int(input('Segundo Número: '))
 
+  contador = 0
+  resultado = 0
+  while contador < n2:
+    contador += 1
+    resultado += n1
+  print(f'O Resultado é {resultado}')
+
+main()
