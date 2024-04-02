ed5aa37a9a8fab0f937a9928c5bbe64839de1187
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-01 23:43:21 -0300

    Update questao22.py

--- questao22.py
+++ questao22.py
@@ -1,1 +1,17 @@
+def main():
+  print('Divisão e Resto sem Quociente\n-------------------------')
+  num1 = int(input('Digite o 1º Número : '))
+  num2 = int(input('Digite o 2º Número : '))
+  
+  quociente = 0
+  resto = num1
+  
+  while resto >= num2:
+      resto -= num2
+      quociente += 1
+  
+  print('Quociente:', quociente)
+  print('Resto:', resto)
+
+main()
 
