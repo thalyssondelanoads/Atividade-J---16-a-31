a6c80653fcab0be2691f2815ca1f54f8bf2b77c8
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-04-01 23:47:57 -0300

    Update questao29.py

--- questao29.py
+++ questao29.py
@@ -1,1 +1,23 @@
+def main():
+  print('Investimento Bancário\n--------------------')
+  continuar = 'S'
+  
+  while continuar.upper() == 'S':
+      saldo_inicial = 0
+      investimento_mensal = float(input('Informe o Valor a ser Investido por Mês: '))
+      taxa_juros_mensal = float(input('Informe a Taxa de Juros Mensal (em %): '))
+  
+      saldo = saldo_inicial
+      meses = 0
+      while meses < 12:
+          saldo += investimento_mensal
+          saldo *= 1 + (taxa_juros_mensal / 100)
+          meses += 1
+  
+      print('Saldo do Investimento após 1 ano:', round(saldo, 2))
+  
+      continuar = input('Deseja Processar Mais um Ano (S/N)? ')
+      if continuar.upper() == 'N':
+        print('Programa Finalizado')
 
+main()
